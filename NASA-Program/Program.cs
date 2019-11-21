0994a184202b6297939faa2f291fd60c47e3d9ab
ephemeral00 <54561805+ephemeral00@users.noreply.github.com>
2019-11-21 14:02:49 -0500

    first main semi-update
    slight update, planning on adding many more variables & such to the two classes, just have a few up right now

--- NASA-Program/Program.cs
+++ NASA-Program/Program.cs
@@ -1,12 +1,61 @@
 ﻿using System;
 
+/*
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
 namespace NASA_Program
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            
+
+
+
         }
     }
+
+
+    class PodProperties
+    {
+
+        //
+
+        double podMass = 3315.3576;
+        double velocityX; // m/s
+        double velocityY; // m/s
+        double podRadius; // m
+        double velocity; // m/s
+
+        double launchHeight; // km
+        const double launchAngle = 0; // degree
+        double impactAngle; // degree
+
+    }
+
+
+    class MoonProperties
+    {
+        const double moonRadius = 1737.12591; // km
+        const double moonCircumference = 10921.01; // km
+        double moonMass = 7.342 * Math.Pow(10, 22); // kg
+        const double moonGravity = 1.625; // m/s
+
+
+        const double moonMinOrbitV = 0.970; // km/s
+        const double moonMaxOrbitV = 1.082; // km/s
+
+
+    }
+
+
 }
