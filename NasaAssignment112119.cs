a1bcd5e0fe6a0b1699ef886fd12f8500e2a7d1a9
Spyrofan1313 <54641536+Spyrofan1313@users.noreply.github.com>
2019-12-06 13:24:14 -0500

    Added Force If Statement

---
+++ NasaAssignment112119.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Force_if_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int force = 0;
+            const int MAX_FORCE = 500;
+
+            if(force > MAX_FORCE)
+            {
+                Console.WriteLine("Supply pod delivery failed. Pod took too much force.");
+            }
+            else
+            {
+                Console.WriteLine("Supply pod delivery successful.");
+            }
+        }
+    }
+}
